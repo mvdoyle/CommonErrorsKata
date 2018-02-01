4b877adf1612678cbb517b5d2e81664ffe5f35ee
Michael Doyle <mvdoyle@uab.edu>
2018-02-01 09:37:12 -0600

    updated refactored code

--- CommonErrorsKata/QuizForm.cs
+++ CommonErrorsKata/QuizForm.cs
@@ -10,33 +10,35 @@ namespace CommonErrorsKata
 {
     public partial class CommonErrorsForm : Form
     {
-        private readonly AnswerQueue<TrueFalseAnswer> answerQueue;
-        private readonly string[] files;
-        private readonly SynchronizationContext synchronizationContext;
-        private int i = 100;
-        private string currentBaseName;
-        private readonly string[] possibleAnswers;
+        private readonly AnswerQueue<TrueFalseAnswer> _answerQueue;
+        private readonly string[] _files;
+        private readonly SynchronizationContext _synchronizationContext;
+        private readonly int _maxAnswers = 2;
+        private int time = 100;
+        private string _visibleImagePath;
+        private readonly string[] _possibleAnswers;
 
         public CommonErrorsForm()
         {
             InitializeComponent();
-            synchronizationContext = SynchronizationContext.Current;
-            files = System.IO.Directory.GetFiles(Environment.CurrentDirectory +  @"..\..\..\ErrorPics");
-            possibleAnswers = new string[] { "Missing File", "null instance", "divide by zero" };
-            lstAnswers.DataSource = possibleAnswers;
-            answerQueue = new AnswerQueue<TrueFalseAnswer>(15);
+            _synchronizationContext = SynchronizationContext.Current;
+            _files = Directory.GetFiles(Environment.CurrentDirectory +  @"..\..\..\ErrorPics");
+            _possibleAnswers = _files.Select(Path.GetFileNameWithoutExtension).ToArray();
+            lstAnswers.DataSource = _possibleAnswers;
+            _answerQueue = new AnswerQueue<TrueFalseAnswer>(_maxAnswers);
             Next();
             lstAnswers.Click += LstAnswers_Click;
             StartTimer();
         }
+
         private async void StartTimer()
         {
             await Task.Run(() =>
             {
-                for (i = 100; i > 0; i--)
+                for (time = 100; time > 0; time--)
                 {
-                    UpdateProgress(i);
-                    Thread.Sleep(50);
+                    UpdateProgress(time);
+                    Thread.Sleep(100);
                 }
                 Message("Need to be quicker on your feet next time!  Try again...");
             });
@@ -44,49 +46,37 @@ namespace CommonErrorsKata
 
         private void LstAnswers_Click(object sender, EventArgs e)
         {
-            i = 100;
+            time = 100;
 
-            var selected = possibleAnswers[(int) lstAnswers.SelectedItem];
-            if (selected != null && selected == (string) lstAnswers.SelectedItem)
-            {
-                answerQueue.Enqueue(new TrueFalseAnswer(true));
-            }
-            else
-            {
-                answerQueue.Enqueue(new TrueFalseAnswer(false));
-            }
+            var selected = _possibleAnswers[lstAnswers.SelectedIndex];
+            _answerQueue.Enqueue(new TrueFalseAnswer(selected == _visibleImagePath));
 
-            var tokens = currentBaseName.Split(' ');
-            //TODO:  Figure out what is a valid answer.
-            answerQueue.Enqueue(new TrueFalseAnswer(true));
             Next();
         }
 
         private void Next()
         {
-            if (answerQueue.Count == 15 && answerQueue.Grade >= 98)
+            if (_answerQueue.Count == _maxAnswers && _answerQueue.Grade >= 98)
             {
-                MessageBox.Show("Congratulations you've defeated me!");
+                MessageBox.Show(@"Congratulations you've defeated me!");
                 Application.Exit();
                 return;
             }
-            label1.Text = answerQueue.Grade.ToString() + "%";
-            var file = files.GetRandom();
-            currentBaseName= Path.GetFileName(file);
+            label1.Text = _answerQueue.Grade + @"%";
+            var file = _files.GetRandom();
+            _visibleImagePath = Path.GetFileNameWithoutExtension(file);
             pbImage.ImageLocation = file;
         }
 
         public void UpdateProgress(int value)
         {
-            synchronizationContext.Post(new SendOrPostCallback(x => {
+            _synchronizationContext.Post(x => {
                 progress.Value = value;
-            }), value);
+            }, value);
         }
         public void Message(string value)
         {
-            synchronizationContext.Post(new SendOrPostCallback(x => {
-                MessageBox.Show(value);
-            }), value);
+           if(value != null) _synchronizationContext.Post(x => { MessageBox.Show(value); }, value);
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
